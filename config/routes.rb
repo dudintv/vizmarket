a3843ed7c6b7bc66cfd0c23baa6ec1d940ece77c
Дмитрий Дудин <svetoz@gmail.com>
2019-12-27 21:14:07 +0100

    setup Users and Admins as device models and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#test'
+  
+  devise_for :admins
+  devise_for :users
 
   get "/profile", to: "profile#index"
   get "/profile/*slug", to: "profile#index"
