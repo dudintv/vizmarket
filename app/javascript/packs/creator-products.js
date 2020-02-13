4e82662c2d337ac47d24227d67b4bec3d0de6038
Дмитрий Дудин <svetoz@gmail.com>
2020-02-13 23:15:20 +0100

    setup vuex

--- app/javascript/packs/creator-products.js
+++ app/javascript/packs/creator-products.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import store from '../creator/store'
 import ProductsApp from '../creator'
 import NewProductApp from '../creator/ProductEditPage/new'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
-    render: h => h(ProductsApp)
+    render: h => h(ProductsApp),
+    store
   }).$mount()
   if (document.getElementById('creator-products-app'))
     document.getElementById('creator-products-app').appendChild(app.$el)
@@ -12,7 +14,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
-    render: h => h(NewProductApp)
+    render: h => h(NewProductApp),
+    store
   }).$mount()
   if (document.getElementById('creator-new-product-app'))
     document.getElementById('creator-new-product-app').appendChild(app.$el)
