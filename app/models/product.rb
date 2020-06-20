dd4d927835e1e25955138a59fabfd03b24ede6be
Dmitry Dudin <dudintv@gmail.com>
2020-06-20 23:44:13 +0200

    add validation Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
   belongs_to :kind
+  
+  validates :title, :kind, :user, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:kind) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:user) }
+  it { should validate_presence_of(:kind) }
 end
