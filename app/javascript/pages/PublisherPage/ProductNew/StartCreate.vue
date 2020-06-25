457b3ba144e7a71a2a9ddbeb59143895ca2a4ded
Dmitry Dudin <dudintv@gmail.com>
2020-06-25 17:01:45 +0200

    Update validation in Creating New Product form

--- app/javascript/pages/PublisherPage/ProductNew/StartCreate.vue
+++ app/javascript/pages/PublisherPage/ProductNew/StartCreate.vue
@@ -2,9 +2,10 @@
   .start-create-new-product.p-4
     h3 Choose type and name to begin
     form
-      RadioInput(name="Type" v-model="kind" :options="kinds")
-      TextInput.w-full(name="New product name" v-model="name" :class="{'field_with_errors': hasError}")
-      #error_explanation(v-if="hasError") {{ error }}
+      RadioInput(name="Type" v-model="kind" :options="kinds" :class="{'field_with_errors': triedToSend && errors.kind !== ''}")
+      TextInput.w-full(name="New product name" v-model="name" :class="{'field_with_errors': triedToSend && errors.name !== ''}")
+      ul#error_explanation(v-if="triedToSend && hasError")
+        li(v-for="error in errors" v-if="error !== ''") {{ error }}
       button.w-full.bg-red-grad-x.hover--bg-red-grad-x-hover.p-4.uppercase(@click.prevent="createProduct") Create new product
 </template>
 
@@ -22,14 +23,18 @@ export default {
       name: '',
       kind: '',
       kinds: ['script', 'scene', 'plugin'],
-      hasError: false,
-      error: ''
+      triedToSend: false,
+      errors: {
+        kind: '',
+        name: '',
+      }
     }
   },
   methods: {
     createProduct () {
-      if (this.name.trim() !== '' && this.kind.trim() !== '') {
-        this.hasError = false
+      this.triedToSend = true
+      this.validateInput()
+      if (this.hasError) {
         this.$backend.products.create({ name: this.name, kind: this.kind })
           .then(response => {
             if (response.status === 201) {
@@ -38,23 +43,22 @@ export default {
           })
           .catch(error => {
             console.log('!! error', error)
-          })
-      } else {
-        this.hasError = true
-        this.checkInput()
+          }) 
       }
     },
-    checkInput() {
-      if (this.hasError) {
-        if (this.name.trim() === '') { this.error = 'Please enter the name to continue' }
-        if (this.kind.trim() === '') { this.error = 'Please choose the kind to continue' }
-        if (this.name.trim() !== '' && this.kind.trim() !== '') { this.hasError = false }
-      }
+    validateInput () {
+      this.errors.kind = this.kind.trim() === '' ? 'Please choose the kind to continue' : ''
+      this.errors.name = this.name.trim() === '' ? 'Please enter the name to continue' : ''
+    }
+  },
+  computed: {
+    hasError () {
+      return (this.errors.kind !== '' || this.errors.name !== '')
     }
   },
   watch: {
-    name () { this.checkInput() },
-    kind () { this.checkInput() },
+    kind() { this.validateInput() },
+    name() { this.validateInput() },
   }
 }
 </script>
