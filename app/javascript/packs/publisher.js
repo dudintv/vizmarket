49b9f576884d0d1c3242d4de06186d0685e532bd
Dmitry Dudin <dudintv@gmail.com>
2020-06-29 00:03:25 +0200

    install vuelidate

--- app/javascript/packs/publisher.js
+++ app/javascript/packs/publisher.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import store from '../pages/PublisherPage/store'
 import router from '../pages/PublisherPage/router'
+import Vuelidate from 'vuelidate'
 import ProductsApp from '../pages/PublisherPage'
 
 import backend from '../backend/axios'
 Vue.prototype.$backend = backend;
+Vue.use(Vuelidate)
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
