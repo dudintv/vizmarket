31b7936837f2a181a8042ecc5515c6ce8cee6aa6
Dmitry Dudin <dudintv@gmail.com>
2020-06-30 14:21:32 +0200

    upgrade text input vue component

--- app/javascript/components/inputs/text.vue
+++ app/javascript/components/inputs/text.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .form-field
     .field-with-label
-      input(type="text" :name="name" :id="name" :value="value" @input="$emit('input', $event.target.value)" placeholder=" ")
+      input(type="text" :name="name" :id="name" :value="value" @input="$emit('input', $event.target.value)" :placeholder="placeholder" :autocomplete="autocomplete")
       label(:for="name"): .input-name {{ name }}
     .tip(v-if="$slots['tip']")
       slot(name="tip")
@@ -14,6 +14,14 @@ export default {
   props: {
     name: String,
     value: String,
+    autocomplete: {
+      type: String,
+      default: "on"
+    },
+    placeholder: {
+      type: String,
+      default: " ",
+    }
   },
 }
 </script>
