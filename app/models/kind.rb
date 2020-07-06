45b7bd1761ec8be49cdd36b52d16c1302d1799c8
Dmitry Dudin <dudintv@gmail.com>
2020-07-06 23:28:41 +0200

    update Kind model

--- app/models/kind.rb
+++ app/models/kind.rb
@@ -1,3 +1,5 @@
 class Kind < ApplicationRecord
   has_many :products
+  
+  validates :title, presence: true, uniqueness: true
 end
--- spec/models/kind_spec.rb
+++ spec/models/kind_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Kind, type: :model do
   it { should have_many(:products) }
+  it { should validate_presence_of(:title) }
+  it { should validate_uniqueness_of(:title) }
 end
