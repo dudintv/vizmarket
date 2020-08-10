6cb2530379c3d3f018c59a9216955655ed01c519
Дмитрий Дудин <svetoz@gmail.com>
2020-08-10 10:40:10 +0200

    install vue-turbolinks

--- app/javascript/packs/publisher.js
+++ app/javascript/packs/publisher.js
@@ -3,12 +3,14 @@ import store from '../pages/PublisherPage/store'
 import router from '../pages/PublisherPage/router'
 import Vuelidate from 'vuelidate'
 import PublisherApp from '../pages/PublisherPage'
+import TurbolinksAdapter from 'vue-turbolinks'
 
 import backend from 'backend/axios'
 Vue.prototype.$backend = backend;
 Vue.use(Vuelidate)
+Vue.use(TurbolinksAdapter)
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   const app = new Vue({
     el: '#publisher-app',
     render: h => h(PublisherApp),
