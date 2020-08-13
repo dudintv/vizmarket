169c876af7ca899f069ce416d36748bc934803c0
Дмитрий Дудин <svetoz@gmail.com>
2020-08-13 13:30:37 +0200

    add Avatar to Users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,11 +2,11 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :surname, :jobtitle, :country, :about
 
-  # attribute :avatar do |object|
-  #   if object.avatar.attached?
-  #     Rails.application.routes.url_helpers.rails_representation_url( object.avatar.variant(resize: "260x260").processed, only_path: true )
-  #   else
-  #     nil
-  #   end
-  # end
+  attribute :avatar do |object|
+    if object.avatar.attached?
+      Rails.application.routes.url_helpers.rails_representation_url( object.avatar.variant(resize: "260x260").processed, only_path: true )
+    else
+      nil
+    end
+  end
 end
