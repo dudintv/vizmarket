348e5c6706a41164e0f9f7505adb52859e69eda5
Дмитрий Дудин <svetoz@gmail.com>
2020-12-21 16:52:31 +0100

    feature: add "copy to clipboard" buttons

--- app/javascript/packs/modal-window.js
+++ app/javascript/packs/modal-window.js
@@ -1,5 +1,6 @@
 import '../components/common/modal/hystmodal.min'
 import '../components/common/modal/hystmodal.min.css'
+import '../components/common/modal/additional.css'
 
 new HystModal({
   linkAttributeName: "data-hystmodal",
