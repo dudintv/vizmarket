0333583e2e22684ad59079d94d3d113a3bcd1cd3
Дмитрий Дудин <svetoz@gmail.com>
2021-03-05 16:50:17 +0100

    feature: add videos to fotorama

--- app/javascript/packs/fotorama.js
+++ app/javascript/packs/fotorama.js
@@ -2,7 +2,19 @@ import '../components/common/fotorama/fotorama'
 import '../components/common/fotorama/fotorama.css'
 
 document.addEventListener('turbolinks:load', () => {
-  // $(function () {
-  //   $('.fotorama').fotorama();
-  // });
+  $(function () {
+    // example of how to improve it — https://codepen.io/pnmcosta/pen/jBeNrm
+    // let fotoramaWidth, fotoramaHeight;
+    $('.fotorama')
+    .on('fotorama:fullscreenenter', () => {
+      // console.log('fullscreenenter')
+    })
+    .on('fotorama:fullscreenexit', () => {
+      // console.log('fullscreenexit')
+    })
+    .fotorama({
+      nav: 'thumbs',
+      fit: 'cover',
+    });
+  });
 })
