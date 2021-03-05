4d8b91b193fe30cde156cdb514bae5926177f30a
Дмитрий Дудин <svetoz@gmail.com>
2021-03-05 15:11:56 +0100

    feature: install and set Fotorama

---
+++ app/javascript/packs/fotorama.js
@@ -0,0 +1,8 @@
+import '../components/common/fotorama/fotorama'
+import '../components/common/fotorama/fotorama.css'
+
+document.addEventListener('turbolinks:load', () => {
+  // $(function () {
+  //   $('.fotorama').fotorama();
+  // });
+})
