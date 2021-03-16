dbbe52f04a4bf3aa926a26c8ac3e9557443c1289
Дмитрий Дудин <svetoz@gmail.com>
2021-03-16 18:57:16 +0100

    fix: production key linking for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :puma_init_active_record, true # Change to false when not using ActiveRecord
 
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/credentials/production.key')
 
 
 namespace :puma do
