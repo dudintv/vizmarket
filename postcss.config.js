e84e79ac777e8fe69a52cfb2d8111a24f21a4a91
Дмитрий Дудин <svetoz@gmail.com>
2021-03-16 21:12:31 +0100

    chore: try to disable part of postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,10 @@ module.exports = {
   syntax: 'postcss-scss',
   plugins: [
     require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('precss'),
+    // require('postcss-flexbugs-fixes'),
+    // require('precss'),
     require('tailwindcss')('./app/javascript/css/tailwind/tailwind.js'),
-    require('autoprefixer'),
+    // require('autoprefixer'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
