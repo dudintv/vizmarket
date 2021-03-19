40c860328f8be39e7e75b01e69f86c1baa070adb
Дмитрий Дудин <svetoz@gmail.com>
2021-03-19 19:20:53 +0100

    test: update for deleting user

--- spec/models/feedback_message_spec.rb
+++ spec/models/feedback_message_spec.rb
@@ -13,9 +13,14 @@ RSpec.describe FeedbackMessage, type: :model do
       expect(User.where(id: user.id).to_a).to eql([])
     end
 
-    it 'keeps feedback messages after user destroing' do
+    it 'keeps feedback_messages after user destroing' do
       feedback_message.user.destroy
       expect(feedback_message.reload).to be
+    end
+
+    it 'nulify user_id in feedback_message' do
+      feedback_message.user.destroy
+      expect(feedback_message.reload.user_id).to be_nil
     end
   end
 end
