ab570536fc50e5fb896ada3f2692298814700199
Дмитрий Дудин <svetoz@gmail.com>
2021-04-08 17:57:25 +0200

    fix: default order of products

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,7 +15,7 @@ class Product < ApplicationRecord
   has_one_attached :featured_image
   has_many_attached :images
 
-  default_scope { where(public: true) }
+  default_scope { where(public: true).order('created_at desc') }
   
   validates :title, :kind, :user, presence: true
 
