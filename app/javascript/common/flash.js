2fecab476b591d1c9ecf7df3d796879e48e65b4e
Дмитрий Дудин <svetoz@gmail.com>
2021-04-08 23:54:33 +0200

    fix: showing toast everywhere

--- app/javascript/common/flash.js
+++ app/javascript/common/flash.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import TurbolinksAdapter from 'vue-turbolinks'
+Vue.use(TurbolinksAdapter)
 import Flash from 'components/common/flash'
-const FlashVM = new Vue(Flash)
-window.FlashVM = FlashVM
+
 
 document.addEventListener("turbolinks:load", function() {
-  FlashVM.$mount('#flash')
+  console.log("[ flash.js turbolinks:load]")
+  // const flashElement = document.getElementById('flash')
+  // console.debug('flashElement = ', flashElement)
+  FlashVM?.$destroy()
+  const FlashVM = new Vue(Flash)
+  FlashVM.$mount('#flash-app')
+  window.FlashVM = FlashVM
 })
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   FlashVM.$mount('#flash-app')
+// })
+
+// FlashVM.unmount('#flash')
+// FlashVM = null
