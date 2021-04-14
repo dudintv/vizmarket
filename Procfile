096b2a9b907d2749629ef95bbcc98a9b472600ec
Дмитрий Дудин <svetoz@gmail.com>
2021-04-14 17:13:44 +0200

    cleanup comments

--- Procfile
+++ Procfile
@@ -1,3 +1,9 @@
+# FULL APP RUN
+# docker-compose up -d postgres; rm .overmind.sock; overmind s
+# 
+# APP RUN:
+# overmind s
+# 
 # postgres: docker-compose up postgres
 rails: export DATABASE_URL=postgres://postgres:postgres@localhost:5400 && rails s -p 5000
 webpacker: ./bin/webpack-dev-server
--- app/javascript/common/flash.js
+++ app/javascript/common/flash.js
@@ -3,20 +3,10 @@ import TurbolinksAdapter from 'vue-turbolinks'
 Vue.use(TurbolinksAdapter)
 import Flash from 'components/common/flash'
 
-
 document.addEventListener("turbolinks:load", function() {
   console.log("[ flash.js turbolinks:load]")
-  // const flashElement = document.getElementById('flash')
-  // console.debug('flashElement = ', flashElement)
   FlashVM?.$destroy()
   const FlashVM = new Vue(Flash)
   FlashVM.$mount('#flash-app')
   window.FlashVM = FlashVM
 })
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   FlashVM.$mount('#flash-app')
-// })
-
-// FlashVM.unmount('#flash')
-// FlashVM = null
