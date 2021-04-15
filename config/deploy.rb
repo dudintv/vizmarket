f36993435d468b30e5065ce676e3c47e5a2d1eca
Дмитрий Дудин <svetoz@gmail.com>
2021-04-15 11:42:59 +0200

    fix: deploy git branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -69,8 +69,8 @@ namespace :deploy do
   desc 'Make sure local git is in sync with remote.'
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/test`
-        puts 'WARNING: HEAD is not the same as origin/test'
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts 'WARNING: HEAD is not the same as origin/master'
         puts 'Run `git push` to sync changes.'
         exit
       end
